71c279c3984b857f047a0b33d8327e5d3c949cbd
umar ilyas <umarilyas595@gmail.com>
2024-07-22 18:19:38 +0500

    node commit

---
+++ test/endpoints.js
@@ -0,0 +1,41 @@
+process.env.NODE_ENV = 'test'
+
+var test = require('ava')
+var servertest = require('servertest')
+
+var server = require('../lib/server')
+
+test.serial.cb('healthcheck', function (t) {
+  var url = '/health'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
+
+
+
+test.serial.cb('get all targets', function (t) {
+  var url = '/api/targets'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
+
+test.serial.cb('get single target', function (t) {
+  var url = '/api/targets/1'
+  servertest(server(), url, { encoding: 'json' }, function (err, res) {
+    t.falsy(err, 'no error')
+
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.status, 'OK', 'status is ok')
+    t.end()
+  })
+})
